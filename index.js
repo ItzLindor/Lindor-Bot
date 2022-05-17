4fc477c9e725b71141fde57bde3351172d01143f
ItzLindor <60286921+ItzLindor@users.noreply.github.com>
2022-05-17 12:53:32 -0400

    testing

--- index.js
+++ index.js
@@ -17,15 +17,6 @@ const client = new Client({
 
 
 
-function sleep(num) {
-	let now = new Date();
-	const stop = now.getTime() + num;
-	while(true) {
-		now = new Date();
-		if(now.getTime() > stop) return;
-	}
-}
-
 
 
 
